befbcfc1f5b69c368bff7dab7defe6b102c768da
whitelemontree <chunjie_zhang@mymail.sutd.edu.sg>
2021-09-23 17:55:23 +0800

    first commit

--- mp_sort/app/static/library.py
+++ mp_sort/app/static/library.py
@@ -4,7 +4,14 @@ import random
 array = []
 
 def gen_random_int(number, seed):
-	pass
+	pack = []
+	for i in range(number):
+		pack.append(i)
+	random.seed(seed)
+	random.shuffle(pack)
+	result = pack
+	return result
+
 
 def generate():
 	global array
@@ -14,21 +21,43 @@ def generate():
 
 	# call gen_random_int() with the given number and seed
 	# store it to the global variable array
-	pass
-
-	array = None
+	array = gen_random_int(number,seed)
 	# convert the items into one single string 
 	# the number should be separated by a comma
 	# and a full stop should end the string.
-	pass
 
-	array_str = None
-
+	array_str = ""
+	for nums in array:
+		array_str = array_str+str(nums)+", "
+	
+	array_str = array_str[:-2:]+"."
 	# This line is to placed the string into the HTML
 	# under div section with the id called "generate"	
 	document.getElementById("generate").innerHTML = array_str
 
-
+def insertion_sort(lst):
+	for index in range(1,len(lst)):
+		swapped = True
+		start = index 
+		while start>0 and swapped:
+			swapped = False
+			if lst[start]<lst[start-1]:
+				lst[start],lst[start-1] =lst[start-1],lst[start]
+				start -= 1
+				swapped = True
+def bubble_sort(lst):
+	n = len(lst)
+	swapped = True
+	while swapped:
+		swapped = False
+		for i in range(1,n):
+			if lst[i-1] > lst[i]:
+				lst[i],lst[i-1] = lst[i-1],lst[i]
+				swapped = True
+		n = n-1
+	
+		
+		
 def sortnumber1():
 	'''	This function is used in Exercise 1.
 		The function is called when the sort button is clicked.
@@ -39,9 +68,12 @@ def sortnumber1():
 		- call your sort function, either bubble sort or insertion sort
 		- create a string of the sorted numbers and store it in array_str
 	'''
-	pass
-
-	array_str = None
+	sorted_array = array
+	insertion_sort(sorted_array)
+	array_str=""
+	for nums in sorted_array:
+		array_str = array_str +str(nums)+", "
+	array_str = array_str[:-2:]+"."
 	
 	document.getElementById("sorted").innerHTML = array_str
 
@@ -66,9 +98,18 @@ def sortnumber2():
 
 	# Your code should start from here
 	# store the final string to the variable array_str
-	pass
-
-	array_str = None
+	lst = value.split(",")
+	lst_nums  = []
+	array_str = ""
+	for i in lst:
+		if i.isdigit():
+			lst_nums.append(int(i))
+		if i[0] == "-" and i[1::].isdigit():
+			lst_nums.append(-1*int(i[1:]))
+	bubble_sort(lst_nums)
+	for nums in lst_nums:
+		array_str = array_str+ nums+", "
+	array_str  = array_str[:-2:]+"."
 
 	document.getElementById("sorted").innerHTML = array_str
 
